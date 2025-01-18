2de66f04a4b67ca1f6b32a5732c436fca5ce9c96
SaiGanesh-Coder <Saiganesh.Kudipudi@gmail.com>
2025-01-18 21:23:04 +0530

    Added 'Workspace Creation'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
-import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+
+import { Modals } from "@/components/modals";
+import { Toaster } from "@/components/ui/sonner";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -33,6 +35,8 @@ export default function RootLayout({
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 					<ConvexClientProvider>
+						<Toaster />
+						<Modals />
 						{children}
 					</ConvexClientProvider>
 				</body>
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -106,7 +106,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 					</Button>
 				</div>
 				<p className="text-xs text-muted-foreground">
-					Don&apos;'t have an account?{" "}
+					Don&apos;t have an account?{" "}
 					<span
 						onClick={() => setState("signUp")}
 						className="text-sky-700 hover:underline cursor-pointer"
